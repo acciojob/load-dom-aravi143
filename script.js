88f03a69ac1373a628762b78427191ba0f23a507
Aravind Chinna <151151715+aravi143@users.noreply.github.com>
2023-12-24 15:25:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
-document.addEventListener(DOMContentLoaded,() =>{
+document.addEventListener('DOMContentLoaded', () => {
 	document.body.textContent = "DOM load success";
 })
